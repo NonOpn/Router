ec81b49699a274cbdab3d83cef4b65d3fa583726
Kevin Le Perf <codlabtech@gmail.com>
2017-12-03 12:15:10 +0100

    add comment for routair

--- update.sh
+++ update.sh
@@ -3,6 +3,9 @@
 #checking for contact-platform connectivity
 #if available, github should then be too
 
+#alternatively, you can use crontab -e to add this script launched
+#e.g. 0 */3 * * * /usr/local/routair
+
 if ping -c 1 contact-platform.com >> /dev/null 2>&1; then
   echo "online, check for updates"
   cd /usr/local/routair
