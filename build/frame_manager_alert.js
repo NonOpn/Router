b502eaadd399365a13f49865586768bbf6262470
codlab <codlabtech@gmail.com>
2020-02-18 18:18:15 +0100

    proper not product

--- build/frame_manager_alert.js
+++ build/frame_manager_alert.js
@@ -34,6 +34,7 @@ class FrameManagerAlert extends events_1.EventEmitter {
     tryUpdateDevicesForContactairs(devices, internal_serials) {
         const to_update = internal_serials.filter(item => {
             const device = this.deviceForInternal(devices, item.internal_serial);
+            console.log("to_update ? ", { device, item });
             if (this.hasProduct(item) && (!device || device.last_contactair))
                 return false;
             return device && device.last_contactair != item.contactair;
