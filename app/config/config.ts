20ac5285abec5e07b0f6f1120f424656f7869945
codlab <codlabtech@gmail.com>
2020-12-19 23:46:12 +0100

    bump to 3.3.4

--- app/config/config.ts
+++ app/config/config.ts
@@ -5,7 +5,7 @@ try {
 } catch(e) {
 
 }
-const VERSION = "3.3.3";//process.env.VERSION || "1.0";
+const VERSION = "3.3.4";//process.env.VERSION || "1.0";
 
 export interface Config {
   identity: string|undefined|null,
