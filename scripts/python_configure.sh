b1964b74f7dbc22969d664793e48c83795f790fd
Kévin Le Perf <codlabtech@gmail.com>
2021-04-14 06:45:27 +0200

    add support for i2c

--- scripts/python_configure.sh
+++ scripts/python_configure.sh
@@ -11,7 +11,7 @@ if [ $found -eq 0 ]; then
   echo "dtparam=i2c_baudrate=10000" >> /boot/config.txt
   echo "dtparam=i2s=on" >> /boot/config.txt
   echo "dtparam=spi=on" >> /boot/config.txt
-  #raspi-config nonint do_i2c 0
+  raspi-config nonint do_i2c 0
 else
   echo "already set"
 fi
