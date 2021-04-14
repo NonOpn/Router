a2e076912811d00c4070b1adef9fe842f5073735
Kévin Le Perf <codlabtech@gmail.com>
2021-04-14 02:12:15 +0200

    move script

--- scripts/python_configure.sh
+++ scripts/python_configure.sh
@@ -8,11 +8,11 @@ if [ $found -eq 0 ]; then
   echo "dtparam=i2c_baudrate=10000" >> /boot/config.txt
   echo "dtparam=i2s=on" >> /boot/config.txt
   echo "dtparam=spi=on" >> /boot/config.txt
+  raspi-config nonint do_i2c 0
 else
   echo "already set"
 fi
 
-raspi-config nonint do_i2c 0
 modprobe -r i2c_bcm2708
 modprobe i2c_bcm2708 baudrate=100000
 
