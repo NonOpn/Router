0510d0acb87aed6dd09113ac5f983b419d3ada60
Kévin Le Perf <codlabtech@gmail.com>
2021-06-04 21:06:48 +0200

    add data reporter

--- app/log/reporter.ts
+++ app/log/reporter.ts
@@ -16,14 +16,25 @@ export default class Reporter {
         setInterval(() => this._report(), 60 * 60 * 1000); //60min * 60s * 1000ms
     }
 
-    private _report() {
-      Diskspace.instance.diskspace()
-      .then(space => {
-        return FrameModel.instance.getCount()
-        .then(count => {
-          Logger.data({ context: "space", space, database: { count } });
-        });
-      })
-      .catch(err => console.log(err));
+    private _report = async () => {
+      var space: any = {};
+      var count: number = 0;
+      try {
+        space = await Diskspace.instance.diskspace();
+      } catch(ee) {
+
+      }
+
+      try {
+        count = await FrameModel.instance.getCount();
+      } catch(e) {
+
+      }
+
+      try {
+        await Logger.data({ context: "space", space, database: { count } });
+      } catch(eee) {
+
+      }
     }
 }
--- build/log/reporter.js
+++ build/log/reporter.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -9,6 +18,25 @@ const frame_model_1 = __importDefault(require("../push_web/frame_model"));
 class Reporter {
     constructor() {
         this.started = false;
+        this._report = () => __awaiter(this, void 0, void 0, function* () {
+            var space = {};
+            var count = 0;
+            try {
+                space = yield system_1.Diskspace.instance.diskspace();
+            }
+            catch (ee) {
+            }
+            try {
+                count = yield frame_model_1.default.instance.getCount();
+            }
+            catch (e) {
+            }
+            try {
+                yield _1.Logger.data({ context: "space", space, database: { count } });
+            }
+            catch (eee) {
+            }
+        });
     }
     start() {
         if (this.started)
@@ -16,16 +44,6 @@ class Reporter {
         this.started = true;
         this._report();
         setInterval(() => this._report(), 60 * 60 * 1000); //60min * 60s * 1000ms
-    }
-    _report() {
-        system_1.Diskspace.instance.diskspace()
-            .then(space => {
-            return frame_model_1.default.instance.getCount()
-                .then(count => {
-                _1.Logger.data({ context: "space", space, database: { count } });
-            });
-        })
-            .catch(err => console.log(err));
     }
 }
 exports.default = Reporter;
